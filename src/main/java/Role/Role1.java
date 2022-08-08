235ad61e0e146ec4e9dd1884445e59a3f3f6f585
주동호 <100278301+JOODON@users.noreply.github.com>
2022-08-08 10:30:54 +0900

    데이터 베이스 생성 및 자바 Role만듬

---
+++ src/main/java/Role/Role1.java
@@ -0,0 +1,31 @@
+package Role;
+
+public class Role1 {
+    private String name;
+    private String phonenumber;
+    private String companyname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getCompanyname() {
+        return companyname;
+    }
+
+    public void setCompanyname(String companyname) {
+        this.companyname = companyname;
+    }
+}
